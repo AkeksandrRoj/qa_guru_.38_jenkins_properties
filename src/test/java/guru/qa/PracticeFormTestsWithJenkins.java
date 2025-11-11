68a0979b89ad172845b6398a1526432eda2daf49
Aleksandr Rozhkov <dir.gefest@gmail.com>
2025-11-11 22:39:37 +0300

    add remote start

--- src/test/java/guru/qa/PracticeFormTestsWithJenkins.java
+++ src/test/java/guru/qa/PracticeFormTestsWithJenkins.java
@@ -20,7 +20,7 @@ public class PracticeFormTestsWithJenkins {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Tag("homeWork")
